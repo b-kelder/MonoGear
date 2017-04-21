f65f4cdb477760ab4a6448a034cda47cb42203aa
Wouter Brookhuis <wouter.brookhuis@student.stenden.com>
2017-04-21 14:35:06 +0200

    Adds loading support for audio

--- MonoGear/ResourceManager.cs
+++ MonoGear/ResourceManager.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Xna.Framework.Content;
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -46,6 +48,20 @@ namespace MonoGear
                     foreach(var asset in type.Value)
                     {
                         RegisterLoadedResource(asset, content.Load<Texture2D>(asset));
+                    }
+                }
+                else if(type.Key == typeof(Song))
+                {
+                    foreach(var asset in type.Value)
+                    {
+                        RegisterLoadedResource(asset, content.Load<Song>(asset));
+                    }
+                }
+                else if(type.Key == typeof(SoundEffect))
+                {
+                    foreach(var asset in type.Value)
+                    {
+                        RegisterLoadedResource(asset, content.Load<SoundEffect>(asset));
                     }
                 }
             }
