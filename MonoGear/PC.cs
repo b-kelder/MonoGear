13a93b4e90bb826f8c7446ce7d5b25e276f42a8a
Bram Kelder <bram-kelder@hotmail.com>
2017-06-07 11:15:27 +0200

    Camera's en PC's

---
+++ MonoGear/PC.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoGear
+{
+    class PC : WorldEntity
+    {
+        public List<CCTV> connectedCameras { get; set; }
+        public float progressPerClick { get; set; }
+
+        private float hackingProgress;
+
+        public PC()
+        {
+            TextureAssetName = "Sprites/birdsheet";
+            Tag = "PC";
+
+            connectedCameras = new List<CCTV>();
+            hackingProgress = 0;
+            progressPerClick = 1;
+        }
+
+        public void HackPC()
+        {
+            //Hack all connected camera's
+            foreach (var CCTV in connectedCameras)
+            {
+                CCTV.Hack();
+            }
+        }
+
+        public override void Update(Input input, GameTime gameTime)
+        {
+            base.Update(input, gameTime);
+
+            if (input.IsButtonPressed(Input.Button.Interact))
+            {
+                hackingProgress += progressPerClick;
+            }
+
+            if (hackingProgress >= 100)
+            {
+                HackPC();
+            }
+        }
+    }
+}
