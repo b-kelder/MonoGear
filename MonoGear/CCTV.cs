cfa135beb4b2aeb85191e7fcaa7c228761b8c7cd
Wouter Brookhuis <wouter.brookhuis@student.stenden.com>
2017-06-07 13:57:59 +0200

    Colorful fov indicators

--- MonoGear/CCTV.cs
+++ MonoGear/CCTV.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,10 @@ namespace MonoGear
 {
     class CCTV : WorldEntity
     {
-        public int sightRange { get; set; }
-        public int sightFov { get; set; }
+        private static Texture2D fovSprite;
+
+        public int SightRange { get; set; }
+        public int SightFOV { get; set; }
 
         private bool hacked;
         private Player player;
@@ -20,16 +23,27 @@ namespace MonoGear
             TextureAssetName = "Sprites/birdsheet";
             Tag = "CCTV";
 
-            sightRange = 400;
-            sightFov = 45;
+            SightRange = 120;
+            SightFOV = 90;
 
             hacked = false;
+
+            LoadContent();
         }
 
         public override void OnLevelLoaded()
         {
             base.OnLevelLoaded();
             player = MonoGearGame.FindEntitiesWithTag("Player")[0] as Player;
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            if(fovSprite == null)
+            {
+                fovSprite = ResourceManager.GetManager().GetResource<Texture2D>("Sprites/fov100");
+            }
         }
 
         public override void Update(Input input, GameTime gameTime)
@@ -54,6 +68,13 @@ namespace MonoGear
             }
         }
 
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+
+            Guard.DrawFOVDebug(spriteBatch, Position, Rotation, player.Position, SightRange, new Color(0, 100, 0, 10));
+        }
+
         public void Hack()
         {
             hacked = true;
@@ -64,11 +85,11 @@ namespace MonoGear
             var dis = Vector2.Distance(Position, player.Position);
 
             //Check if player is within view range
-            if (dis < sightRange)
+            if (dis < SightRange)
             {
                 //Check to see if the guard is looking at the player
                 var degrees = Math.Abs(MathHelper.ToDegrees(Rotation) - (90 + MathHelper.ToDegrees(MathExtensions.AngleBetween(Position, player.Position))));
-                if (degrees <= (sightFov / 2) || degrees >= (360 - (sightFov / 2)))
+                if (degrees <= (SightFOV / 2) || degrees >= (360 - (SightFOV / 2)))
                 {
                     //Check to see if nothing blocks view of the player
                     Collider hit;
