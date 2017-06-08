b22dde1eb6298ffff83938ffd2463ed781da964b
Bram Kelder <bram-kelder@hotmail.com>
2017-06-08 10:15:42 +0200

    James Bond

--- MonoGear/GameUI.cs
+++ MonoGear/GameUI.cs
@@ -24,26 +24,28 @@ namespace MonoGear
             var rect = Camera.main.GetClippingRect();
             var pos = rect.Right - 100;
 
-            for (int i = 0; i < player.Health; i++)
+            if (!SettingsPage.GetDifficulty().Equals(DifficultyLevels.JamesBond))
             {
-                spriteBatch.Draw(ResourceManager.GetManager().GetResource<Texture2D>("Sprites/Heart"), new Vector2(pos, rect.Bottom - 50), Color.White);
-                pos += 15;
-            }
-
-            if (player.DartCount <= 6)
-            {
-                pos = rect.Right - 100;
-                for (int i = 0; i < player.DartCount; i++)
+                for (int i = 0; i < player.Health; i++)
                 {
-                    spriteBatch.Draw(ResourceManager.GetManager().GetResource<Texture2D>("Sprites/SleepDart"), new Vector2(pos, rect.Bottom - 32), Color.White);
+                    spriteBatch.Draw(ResourceManager.GetManager().GetResource<Texture2D>("Sprites/Heart"), new Vector2(pos, rect.Bottom - 50), Color.White);
                     pos += 15;
                 }
+
+                if (player.DartCount <= 6)
+                {
+                    pos = rect.Right - 100;
+                    for (int i = 0; i < player.DartCount; i++)
+                    {
+                        spriteBatch.Draw(ResourceManager.GetManager().GetResource<Texture2D>("Sprites/SleepDart"), new Vector2(pos, rect.Bottom - 32), Color.White);
+                        pos += 15;
+                    }
+                }
+                else
+                {
+                    spriteBatch.DrawString(ResourceManager.GetManager().GetResource<SpriteFont>("Fonts/Arial"), "Darts: " + player.DartCount, new Vector2(rect.Right - 100, rect.Bottom - 32), Color.Red);
+                }
             }
-            else
-            {
-                spriteBatch.DrawString(ResourceManager.GetManager().GetResource<SpriteFont>("Fonts/Arial"), "Darts: " + player.DartCount, new Vector2(rect.Right - 100, rect.Bottom - 32), Color.Red);
-            }
-            
             //spriteBatch.DrawString(ResourceManager.GetManager().GetResource<SpriteFont>("Fonts/Arial"), "Health: " + player.Health, new Vector2(rect.Right - 100, rect.Bottom - 50), Color.Red);
         }
     }
